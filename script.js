8d7d17138dc47d7765acade52e300974d2a6c7e4
Abidur Nurohi <abidurnurohi@gmail..com>
2025-01-16 18:14:23 +0600

    deploy

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add simple interaction if needed (e.g., form validation, animation, etc.)
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting me!');
+  });
+  
